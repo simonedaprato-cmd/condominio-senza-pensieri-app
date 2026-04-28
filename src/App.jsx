bf4eae9593333bef073f25301a6335f2597d24c9
simonedaprato-cmd <simone.daprato@gmail.com>
2026-04-28 14:40:22 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,12 @@ function buildPublicUrl(fileName) {
 }
 
 function formatEuro(value) {
-  return new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' }).format(Number(value || 0));
+  return new Intl.NumberFormat('it-IT', {
+    style: 'currency',
+    currency: 'EUR',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(Number(value || 0));
 }
 
 function badgeClass(stato) {
